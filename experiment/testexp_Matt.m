49f09732b247e906a3bbb8efe1c522584f198969
B80A Behavior Dell <schafferlab@gmail.com>
2021-03-26 16:16:33 -0400

    Update testexp_Matt.m

--- experiment/testexp_Matt.m
+++ experiment/testexp_Matt.m
@@ -1,11 +1,18 @@
-%open connection to controller 
+%stet experiment parameters
 clear mmc
-mmc.port = 'COM4';
-mmc.controller = serialport(mmc.port,9600);
-% mmc.port = '/dev/cu.usbmodem14201';
-%set experiment parameters
+mmc.primaryPort = 'COM3';
+mmc.displayPorts = {'COM4','COM5','COM6'};
+rewardPos = 3;
+startPos = 0;
 num_trials = 30;
-num_channels = 6; %number of data channels (not sure how many we need yet)
+num_channels = 6; %number of data channels
+
+%open connections to controllers 
+mmc.primarySerial = serialport(mmc.primaryPort,9600);
+numDisplayPorts = length(mmc.displayPorts);
+for p = 1:numDisplayPorts
+    mmc.displaySerial(p) = serialport(mmc.displayPorts{p},9600);
+end
 
 %create data and metadata struct
 script_path = mfilename('fullpath');
@@ -19,18 +26,18 @@ exp.metadata = app.metadata;
            
 % run experiment
 for t = 1:num_trials
-    %send stimulus to display at cue position
-    
-    write(mmc.controller, [100, 0], 'uint8');
-    pause(10);
+    %send stimulus to display at start and reward position
+    write(mmc.displaySerial(startPos+1), [100, 0], 'uint8');
+    write(mmc.displaySerial(rewardPos+1), [100, 0], 'uint8');
 
-    %wait for poke at cue position
-    while mmc.controller.NumBytesAvailable==0 || read(mmc.controller,1,'uint8')~=10
+    %wait for poke at either reward or start position
+    while mmc.primarySerial.NumBytesAvailable==0 || read(mmc.primarySerial,1,'uint8')~=10
         pause(0.01);
     end
-    tic
-    %give a reward at cue position
-    write(mmc.controller, [101, 0], 'uint8');
+    
+    %give a reward at the poked position
+    pokedPos = read(mmc.primarySerial,1,'uint8');
+    write(mmc.primarySerial, [101, pokedPos], 'uint8');
    
     %send stimulus to reward position
     write(mmc.controller, [100, 3], 'uint8');
